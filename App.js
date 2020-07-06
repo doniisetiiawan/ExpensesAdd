731cbb0314edc8353f81198e2215bb1b407ec811
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-06 09:18:25 +0700

    Getting started

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +11,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app!
+      </Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
